246efd6bb763d18f23d75495797b7c99d167b8a1
Tad Vela <tadvela@Tads-MBP.lan>
2025-01-02 10:52:17 -0500

    first commit + home controller/page

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/users'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+end
