7cb8d806444f04954f57fccefeff464bb97ac23d
Tad Vela <tadvela@Tads-MBP.lan>
2025-01-05 21:06:10 -0500

    add bootstrap, header, fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+  #get 'home#index'
   get 'home/users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
