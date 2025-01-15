4162f852c89c5b170e8e55ed38a0fd6d6cd674a8
Tad Vela <tadvela@Tads-MBP.lan>
2025-01-14 21:45:00 -0500

    add tos agreement

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_acceptance_of :tos_agreement, allow_nil: false, on: :create
 end
