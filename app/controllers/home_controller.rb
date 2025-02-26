e5d5cd63a61ca52d3835fb3ebcb5e3df653af609
Tad Vela <tadvela@Tads-MBP.lan>
2025-02-26 12:56:01 -0500

    add conditional to url to qr code and edit survey which to wish

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:terms_of_service, :patient_survey, :submit_form]
-  protect_from_forgery with: :null_session, only: [:submit_form]
+  protect_from_forgery with: :null_session, only: [:submit_form, :users]
 
   def index
   end
@@ -8,15 +8,18 @@ class HomeController < ApplicationController
   def users
 
     #Now creating a QR Code with above products details
-    content = "Hello World."
+    content = Rails.env.development? ? "http://localhost:3000/users/sign_up" : "https://sheltered-chamber-99174-f75c82d999a8.herokuapp.com/users/sign_up"
 
     #For PNG image
     @qr_png = RQRCode::QRCode.new(content).as_png
 
-    send_data @qr_png,
+    send_data(
+      @qr_png,
       type: "image/png",
       disposition: "attachment",
       filename: "qr_code.png"
+    )
+
   end
 
   def terms_of_service
