e50e130d7c2b13329be2c9e4a3525dee1e11a029
Tad Vela <tadvela@Tads-MBP.lan>
2025-03-23 19:43:52 -0400

    set default role to admin upon create

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :user
+    self.role ||= 1
   end
 
   def self.to_csv
